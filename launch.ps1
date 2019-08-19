97762be4df3960aecb82d46c089f601960314860
David Cano <davidcanom@outlook.com>
2019-08-19 02:12:54 -0500

    fix script name

--- launch.ps1
+++ launch.ps1
@@ -1,4 +1,4 @@
-$ScriptLocation = "install-modules.ps1"
+$ScriptLocation = "base-modules.ps1"
 Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File $ScriptLocation" -NoNewWindow -Wait
 
 $ScriptLocation = "dsc_jenkins-master.ps1"
